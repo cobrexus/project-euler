9335eb092a937679e41572bd4830348814f0de4d
cobrexus <aryanbeezadhur@gmail.com>
2025-02-19 18:46:16 +0000

    Edit .clang-format

--- cpp/02.cpp
+++ cpp/02.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     int a{1};
     int b{2};
     int c{0};
 
-    while (b < 4'000'000)
-    {
-        if (b % 2 == 0)
-        {
+    while (b < 4'000'000) {
+        if (b % 2 == 0) {
             c += b;
         }
 
