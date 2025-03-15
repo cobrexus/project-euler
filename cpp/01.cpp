c5cbb4453ba3c786553cf89689c629a5937119c8
cobrexus <aryanbeezadhur@gmail.com>
2025-03-15 18:01:29 +0000

    Clean up C++

--- cpp/01.cpp
+++ cpp/01.cpp
@@ -3,11 +3,9 @@
 int main() {
     int sum{0};
 
-    for (int i{0}; i < 1000; ++i) {
-        if (i % 3 == 0 || i % 5 == 0) {
+    for (int i{0}; i < 1000; ++i)
+        if (i % 3 == 0 || i % 5 == 0)
             sum += i;
-        }
-    }
 
     std::cout << sum << '\n';
 }
--- cpp/02.cpp
+++ cpp/02.cpp
@@ -4,13 +4,13 @@ int main() {
     int a{1};
     int b{2};
     int c{0};
+    int temp{};
 
     while (b < 4'000'000) {
-        if (b % 2 == 0) {
+        if (b % 2 == 0)
             c += b;
-        }
 
-        int temp{a};
+        temp = a;
         a = b;
         b += temp;
     }
